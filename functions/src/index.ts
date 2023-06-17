51fdade32ba96017d87bd64c9a25f8936ba9a5ce
ufoo68 <mitarou.yuutarou@gmail.com>
2023-06-18 00:49:25 +0900

    スプレッドシートの参照先を変更

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,47 +4,56 @@ import { google } from "googleapis";
 import dayjs = require("dayjs");
 
 const auth = new google.auth.GoogleAuth({
+  keyFile: "service-account.json",
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
 const spreadsheetId = process.env.SHEET_ID;
-const sheetName = process.env.SHEET_NAME;
 
-const getSensorData = async (): Promise<string[][]> => {
+const getDeviceState = async (
+  deviceId: string
+): Promise<{
+  deviceId: string;
+  timestamp: Date;
+  distance: number;
+  pressure: number;
+} | undefined> => {
   try {
     const sheets = google.sheets({ version: "v4", auth });
     const res = await sheets.spreadsheets.values.get({
       spreadsheetId,
-      range: `${sheetName}`,
+      range: "device_state",
     });
-    return res?.data?.values ?? [];
+    const data = res?.data?.values ?? [];
+    const body = data.slice(1);
+    const devices = body.map((row) => {
+      return {
+        deviceId: row[0],
+        distance: Number(row[2]),
+        pressure: Number(row[3]),
+        timestamp: new Date(row[4]),
+      };
+    });
+    return devices.find((d) => d.deviceId === deviceId);
   } catch (error) {
     logger.error(error);
-    return [];
+    return undefined;
   }
 };
 
-export const gomiMonitor = onRequest(async (_req, response) => {
-  const data = await getSensorData();
-  if (data.length === 0) {
-    response.send(data);
+export const gomiMonitor = onRequest(async (req, response) => {
+  const deviceId = req.query.deviceId as string;
+  if (!deviceId) {
+    response.status(400).send("Bad Request");
     return;
   }
-  const body = data.slice(1);
-  let newestData = body[0];
-  for (let i = 1; i < data.length; i++) {
-    const currentDate = new Date(`${newestData[0]} ${newestData[1]}`);
-    const loopDate = new Date(data[i][0] + " " + data[i][1]);
-
-    if (loopDate > currentDate) {
-      newestData = data[i];
-    }
+  const data = await getDeviceState(deviceId);
+  if (!data) {
+    response.status(404).send("Not Found");
+    return;
   }
   response.send({
-    timestamp: dayjs(
-      new Date(`${newestData[0]} ${newestData[1]}`)
-    ).toISOString(),
-    distance: newestData[2],
-    pressure: newestData[3],
+    ...data,
+    timestamp: dayjs(data.timestamp).toISOString(),
   });
 });
