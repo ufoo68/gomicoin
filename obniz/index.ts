974195e33841df9497a79f3c23734b3e9cfc4059
ufoo68 <mitarou.yuutarou@gmail.com>
2023-07-15 13:28:07 +0900

    fetch data on first

--- obniz/index.ts
+++ obniz/index.ts
@@ -1,5 +1,16 @@
-import Obniz from "obniz";
+import Obniz, { M5StickC } from "obniz";
 import { getDeviceStatus, getDevices, getGomiToken } from "./client";
+
+const displayState = async (obniz: M5StickC, deviceId: string) => {
+  const deviceState = await getDeviceStatus(deviceId);
+  const gomiToken = await getGomiToken(deviceId);
+  obniz.display?.clear();
+  obniz.display?.print(
+    `\ncapacity: ${(deviceState.capacity * 100).toFixed(2)}%\ntrash: ${
+      deviceState.trash
+    }\n${gomiToken.symbol}: ${gomiToken.amount}`
+  );
+};
 
 const main = async () => {
   const devices = await getDevices();
@@ -7,19 +18,11 @@ const main = async () => {
     const obniz = new Obniz.M5StickC(device.obnizId);
     obniz.onconnect = () => {
       console.log("connected: " + obniz.id);
+      displayState(obniz, device.deviceId);
       obniz.buttonA.onchange = async (state) => {
         if (state) {
           try {
-            const deviceState = await getDeviceStatus(device.deviceId);
-            const gomiToken = await getGomiToken(device.deviceId);
-            obniz.display.clear();
-            obniz.display.print(
-              `\ncapacity: ${(deviceState.capacity * 100).toFixed(
-                2
-              )}%\ntrash: ${deviceState.trash}\n${gomiToken.symbol}: ${
-                gomiToken.amount
-              }`
-            );
+            displayState(obniz, device.deviceId);
           } catch (error) {
             console.error(error);
             obniz.display.clear();
